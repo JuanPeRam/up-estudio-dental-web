34719f57592108a58c26b649a097f4b7d2a35ea7
Juan Pereira <juan.cpereira@hotmail.com>
2024-03-11 10:50:28 +0100

    Added gallery

---
+++ src/db/gallery.js
@@ -0,0 +1,114 @@
+const images_path = 'img/UP-Gallery';
+
+const gallery = [
+    {
+        name: 'Bathroom of the clinic',
+        src: `${images_path}/Bathroom.jpg`
+    },
+    {
+        name: 'Bathroom2.jpg',
+        src: `${images_path}/Bathroom2.jpg`
+    },
+    {
+        name: 'Bathroom3.jpg',
+        src: `${images_path}/Bathroom3.jpg`
+    },
+    {
+        name: 'Box1.jpg',
+        src: `${images_path}/Box1.jpg`
+    },
+    {
+        name: 'Box2.jpg',
+        src: `${images_path}/Box2.jpg`
+    },
+    {
+        name: 'Box12.jpg',
+        src: `${images_path}/Box12.jpg`
+    },
+    {
+        name: 'Box22.jpg',
+        src: `${images_path}/Box22.jpg`
+    },
+    {
+        name: 'Box23.jpg',
+        src: `${images_path}/Box23.jpg`
+    },
+    {
+        name: 'Boxes.jpg',
+        src: `${images_path}/Boxes.jpg`
+    },
+    {
+        name: 'Entry2.jpg',
+        src: `${images_path}/Entry2.jpg`
+    },
+    {
+        name: 'Entry3.jpg',
+        src: `${images_path}/Entry3.jpg`
+    },
+    {
+        name: 'Kitchen.jpg',
+        src: `${images_path}/Kitchen.jpg`
+    },
+    {
+        name: 'Kitchen2.jpg',
+        src: `${images_path}/Kitchen2.jpg`
+    },
+    {
+        name: 'Kitchen3.jpg',
+        src: `${images_path}/Kitchen3.jpg`
+    },
+    {
+        name: 'Kitchen4.jpg',
+        src: `${images_path}/Kitchen4.jpg`
+    },
+    {
+        name: 'Mchine.jpg',
+        src: `${images_path}/Mchine.jpg`
+    },
+    {
+        name: 'Office.jpg',
+        src: `${images_path}/Office.jpg`
+    },
+    {
+        name: 'Office2.jpg',
+        src: `${images_path}/Office2.jpg`
+    },
+    {
+        name: 'Rest-room.jpg',
+        src: `${images_path}/Rest-room.jpg`
+    },
+    {
+        name: 'Up-Board.jpg',
+        src: `${images_path}/Up-Board.jpg`
+    },
+    {
+        name: 'Up-Boxes.jpg',
+        src: `${images_path}/Up-Boxes.jpg`
+    },
+    {
+        name: 'Up-Cabin.jpeg',
+        src: `${images_path}/Up-Cabin.jpeg`
+    },
+    {
+        name: 'Up-Chairs.jpg',
+        src: `${images_path}/Up-Chairs.jpg`
+    },
+    {
+        name: 'Up-Entry.jpg',
+        src: `${images_path}/Up-Entry.jpg`
+    },
+    {
+        name: 'Uplogo.webp',
+        src: `${images_path}/Uplogo.webp`
+    },
+    {
+        name: 'Up-Machine.jpg',
+        src: `${images_path}/Up-Machine.jpg`
+    },
+    {
+        name: 'Up-WaitRoom.jpg',
+        src: `${images_path}/Up-WaitRoom.jpg`
+    }
+];
+
+export default gallery;
